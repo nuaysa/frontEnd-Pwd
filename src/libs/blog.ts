212ca055476fa838fc61b746f858f326c4d09f5a
Allysa <nuaysa@gmail.com>
2024-11-07 14:52:07 +0700

    blog

--- src/libs/blog.ts
+++ src/libs/blog.ts
@@ -6,7 +6,7 @@ import resolveResponse from "contentful-resolve-response"
 
 export const getBlogs = async() => {
     const res = await fetch (`${base_url}/spaces/${spaceId}/environments/master/entries?access_token=${token}&content_type=blog`
-    ,{next: {revalidate:3600}}
+    ,{next: {revalidate:60}}
     )
     const data = await res.json()
     const result = resolveResponse(data)
@@ -16,7 +16,7 @@ export const getBlogs = async() => {
 
 export const getBlogSlug = async(slug: string) => {
     const res = await fetch (`${base_url}/spaces/${spaceId}/environments/master/entries?access_token=${token}&content_type=blog&fields.slug=${slug}`
-    ,{next: {revalidate:3600}}
+    ,{next: {revalidate:60}}
     )
     const data = await res.json()
     const result = resolveResponse(data)
